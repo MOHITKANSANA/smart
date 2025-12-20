082a84427674b2399afeb5ed518fca1d9f55f3d8
Mohit Kansana <mohitkansana82@gmail.com>
2025-12-20 12:44:46 +0000

    const firebaseConfig = {   apiKey: "AIzaSyDm3XMh5Z6oRGRY4cO7acmElpZb1QMK

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9900881569-43fce",
-  "appId": "1:309386996314:web:ccd5d1b8ede7b09db6c663",
-  "apiKey": "AIzaSyAaok6g86pzyMKq1PU2CCvtFIemmIUsxkQ",
-  "authDomain": "studio-9900881569-43fce.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "309386996314"
+  apiKey: "AIzaSyDm3XMh5Z6oRGRY4cO7acmElpZb1QMKprc",
+  authDomain: "studio-9900881569-43fce.firebaseapp.com",
+  projectId: "studio-9900881569-43fce",
+  storageBucket: "studio-9900881569-43fce.firebasestorage.app",
+  messagingSenderId: "309386996314",
+  appId: "1:309386996314:web:ccd5d1b8ede7b09db6c663"
 };
